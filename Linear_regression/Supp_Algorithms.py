Fix bug in the Features Normalization function, and save the mean and the standard deviation to be used in the predication

Linear_regression/Supp_Algorithms.py
@@ -29,8 +29,10 @@ class DataPreparation:
         :param Xi: a vector contain the feature value
         :return: normalized the feature vector
         """
-
-        return (Xi - np.mean(Xi)) / np.std(Xi)
+        x_mean = np.mean(Xi)
+        x_std = np.std(Xi)
+        #
+        return (Xi - x_mean) / x_std, np.array([x_mean, x_std])
 
     @staticmethod
     def featuresNormaliz(X):
@@ -41,9 +43,12 @@ class DataPreparation:
         """
         row, col = X.shape
         norm_X = np.array(X)
-        for colIdx in range(col, 1):
-            norm_X[:, colIdx] = DataPreparation.featureNormalize(X[:, colIdx])
+        stat_X = np.zeros(shape=(2, col))
+        for colIdx in range(1, col):
+            norm_X[:, colIdx], tmp = DataPreparation.featureNormalize(X[:, colIdx])
+            print(stat_X[:, colIdx])
+            stat_X[:, colIdx] = tmp
 
-        return norm_X
+        return norm_X, stat_X
 
 

Linear_regression/linear_regression.py
@@ -53,7 +53,7 @@ class LinearRegression:
         :return: the function return two information:
                         1- The first output is the model weights after the converge
                         2- The second output is the predication target values
-                     @todo Return the mean and the standard deviation for the descriptive features
+                        3- Save the mean and the standard deviation for the descriptive features
         """
         row, col = self.X.shape
         m = row
@@ -61,7 +61,7 @@ class LinearRegression:
         # [a, b), b > a multiply the output of random_sample by (b-a) and add a:
         self.W = (randB - randA) * np.random.random((1, col)) + randA
         # Normalize the features
-        norm_X = supp.DataPreparation.featuresNormaliz( self.X)
+        norm_X, self.stat_X = supp.DataPreparation.featuresNormaliz(self.X)
 
 
         for it in range(numofIteration):

Linear_regression/main.py
@@ -8,7 +8,7 @@ import numpy as np
 def Test1():
     dataSet = read_dataset.DataSet('./DataSet/franchise_rest.csv')
     learningRate = 0.01
-    numofIteration = 1500
+    numofIteration = 500
     samples_values_arr, target_feature_arr = dataSet.getDataSetAsNumPy()
 
     linearReg = LinearRegression(samples_values_arr, target_feature_arr)
@@ -37,7 +37,7 @@ def Test1():
 def Test2():
     dataSet = read_dataset.DataSet('./DataSet/rental_house.csv')
 
-    learningRate = 0.000001
+    learningRate = 0.1
     numofIteration = 100
     samples_values_arr, target_feature_arr = dataSet.getDataSetAsNumPy()
 
@@ -66,8 +66,8 @@ def Test2():
 
 def Test3():
     dataSet = read_dataset.DataSet('./DataSet/House_Price.csv')
-    learningRate = 0.00000001
-    numofIteration = 1000
+    learningRate = 0.01
+    numofIteration = 500
     samples_values_arr, target_feature_arr = dataSet.getDataSetAsNumPy()
 
     linearReg = LinearRegression(samples_values_arr, target_feature_arr)
@@ -77,18 +77,15 @@ def Test3():
     ax = Axes3D(fig)
 
     ax.scatter3D(samples_values_arr[:, 0], samples_values_arr[:, 1], np.transpose(target_feature_arr),
-                    marker='x')
+                    marker='x', label="The actual Values")
     ax.scatter3D(samples_values_arr[:, 0], samples_values_arr[:, 1], regressionLine,
-                    marker='o')
+                    marker='o', label="The predicated Values")
 
     ax.set_xlabel(dataSet.feature_name[0])
     ax.set_ylabel(dataSet.feature_name[1])
     ax.set_zlabel(dataSet.target_name)
-    # plt.xlabel(dataSet.feature_name[0])
-    # plt.ylabel(dataSet.target_name)
-    #
-    # plt.plot(samples_values_arr, regressionLine)
-    # plt.title(dataSet.data_set_name + ' data set')
+
+    plt.title(dataSet.data_set_name + ' data set')
     print(linearReg.square_error)
     # #
     plt.figure(2)